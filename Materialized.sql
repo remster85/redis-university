ea7a96e054ef8fd2896cbad814ed5f3ef7f0057f
remster85 <remi.desreumaux@gmail.com>
2024-04-18 11:49:05 -0400

    Update Materialized.sql

--- Materialized.sql
+++ Materialized.sql
@@ -3,17 +3,21 @@ SELECT column_name,
        array_agg(DISTINCT value) AS unique_values
 FROM (
     SELECT column_name,
-           unnest(string_to_array(trim(trailing ')' from trim(leading 'ARRAY[' from column_default)), ',')) AS value
-    FROM information_schema.columns
-    WHERE table_name = 'your_table_name'
-    AND data_type = 'ARRAY'
+           value
+    FROM (
+        SELECT column_name,
+               unnest(array_agg(DISTINCT column_name)) AS value
+        FROM information_schema.columns
+        WHERE table_name = 'your_table_name'
+        GROUP BY column_name
 
-    UNION ALL
+        UNION ALL
 
-    SELECT column_name,
-           column_default AS value
-    FROM information_schema.columns
-    WHERE table_name = 'your_table_name'
-    AND data_type != 'ARRAY'
-) subquery
+        SELECT column_name,
+               unnest(array_agg(DISTINCT column_default)) AS value
+        FROM information_schema.columns
+        WHERE table_name = 'your_table_name'
+        GROUP BY column_name
+    ) AS sub
+) AS subquery
 GROUP BY column_name;
