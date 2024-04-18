7e8e0c72ea02a227c2768dc41fbd742de036b988
remster85 <remi.desreumaux@gmail.com>
2024-04-18 10:44:01 -0400

    Update Materialized.sql

--- Materialized.sql
+++ Materialized.sql
@@ -4,4 +4,10 @@ SELECT column_name,
 FROM (
     SELECT column_name,
            CASE 
-               WHEN data_type = 'ARRAY' THEN unnest(string_to_array(column_default,
+               WHEN data_type = 'ARRAY' THEN unnest(string_to_array(column_default, ','))::text
+               ELSE column_default
+           END AS column_value
+    FROM information_schema.columns
+    WHERE table_name = 'your_table_name'
+) subquery
+GROUP BY column_name;
