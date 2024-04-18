9569204e0e8b2c4988176e8a05319aae25069157
remster85 <remi.desreumaux@gmail.com>
2024-04-18 10:39:22 -0400

    Create Materialized.sql

---
+++ Materialized.sql
@@ -0,0 +1,13 @@
+CREATE MATERIALIZED VIEW unique_field_values AS
+SELECT column_name,
+       array_agg(DISTINCT column_value) AS unique_values
+FROM (
+    SELECT column_name,
+           CASE 
+               WHEN data_type = 'ARRAY' THEN unnest(column_default::text::ARRAY)
+               ELSE column_default::text
+           END AS column_value
+    FROM information_schema.columns
+    WHERE table_name = 'your_table_name'
+) subquery
+GROUP BY column_name;
