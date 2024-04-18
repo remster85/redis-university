598c6b6c6fd8e020d79470093715a9791098232d
remster85 <remi.desreumaux@gmail.com>
2024-04-18 11:44:35 -0400

    Update Materialized.sql

--- Materialized.sql
+++ Materialized.sql
@@ -3,11 +3,17 @@ SELECT column_name,
        array_agg(DISTINCT column_value) AS unique_values
 FROM (
     SELECT column_name,
-           CASE 
-               WHEN data_type = 'ARRAY' THEN unnest(string_to_array(column_default, ','))::text
-               ELSE column_default
-           END AS column_value
+           unnest(string_to_array(column_default, ','))::text AS column_value
     FROM information_schema.columns
     WHERE table_name = 'your_table_name'
+    AND data_type = 'ARRAY'
+
+    UNION ALL
+
+    SELECT column_name,
+           column_default::text AS column_value
+    FROM information_schema.columns
+    WHERE table_name = 'your_table_name'
+    AND data_type != 'ARRAY'
 ) subquery
 GROUP BY column_name;
